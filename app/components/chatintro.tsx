fb361782ec7e8c242ddf8de1d901366eba5f46e4
void_sun <johnnymarque1@gmail.com>
2024-11-29 11:21:46 -0300

    Final

--- app/components/chatintro.tsx
+++ app/components/chatintro.tsx
@@ -4,7 +4,7 @@ export default function ChatIntro() {
     return (
         <div className="flex w-full h-full flex-col items-center justify-center">
             <Image src='New_Start.svg' alt="n" width={500} height={500}></Image>
-            <h1 className="font-bold text-4xl text-[#241034]">O Universo na palma da sua mão</h1>
+            <h1 className="font-bold text-4xl text-[#241034]">Universo inteiro na palma da mão.</h1>
         </div>
     )
 }
